915126414ef9742399051b0a04641c7b056cdbca
NOTMORSE <andreicondino2@gmail.com>
2023-02-24 08:07:44 +0800

    second commit

--- app/src/main/java/com/example/activity1/activity01-f.kt
+++ app/src/main/java/com/example/activity1/activity01-f.kt
@@ -1,54 +1,28 @@
 package com.example.activity1
 
+import java.util.*
 
 
 fun main() {
-    main3()
+    var uniqueChars = ""
+    var str1: String
+    var str2: String
+
+    while (true) {
+        print("Enter string 1 (or type 'exit' to quit): ")
+        str1 = readLine() ?: ""
+        if (str1.lowercase(Locale.getDefault()) == "exit") break
+
+        println("Enter string 2: ")
+        str2 = readLine() ?: ""
+
+        for (char in str1 + str2) {
+            if (char !in uniqueChars) {
+                uniqueChars += char
+            }
+        }
+
+        print("Unique characters: $uniqueChars\n")
+        uniqueChars = ""
+    }
 }
- fun main3(){
-        println("Please Enter two names you can think of : ")
-        print("(1)Subject: ")
-        val str = readln()
-        print("(2)Subject: ")
-        val str1 = readln()
-
-        println("String 1: $str")
-        println("String 2: $str1")
-        val str3 = str + str1
-        val str5 = str3.replaceRange(0, 1, str[0].uppercaseChar().toString())
-        println("Unique: $str5")
-        println("Do you want to try again?")
-        print("Y/N: ")
-        val str4 = readln()
-        if (str4 == "Y") {
-            main2()
-        } else {
-            println("Thank you")
-        }
-    }
-    fun main2() {
-        println("Please Enter two names you can think of : ")
-        print("(1)Subject: ")
-        val str = readln()
-        print("(2)Subject: ")
-        val str1 = readln()
-
-        println("String 1: $str")
-        println("String 2: $str1")
-        val str3 = str + str1
-        val str5 = str3.replaceRange(0, 1, str[0].uppercaseChar().toString())
-        println("Unique: $str5")
-        println("Do you want to try again?")
-        print("Y/N: ")
-        val str4 = readln()
-        if (str4 == "Y") {
-            main3()
-        } else {
-            println("Thank you")
-        }
-    }
-
-
-
-
-
